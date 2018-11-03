6d17a93bf8451a38bc2d48aafc6c2f58e783e89c
f2015537 <divyampatro1997@gmail.com>
2018-11-03 10:00:10 +0530

    Added email validation

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,8 @@ module.exports = {
     "extends": "standard",
     "rules": {
     	"no-path-concat": 0,
-    	"handle-callback-err": 0
+        "handle-callback-err": 0,
+        
+        "no-useless-escape": 0
     }
 };
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var conString = 'mongodb://anumeha:systers2018@ds217131.mlab.com:17131/demo-inviter'
 var express = require('express')
+const validate = require('./validation')
 var app = express()
 const PORT = process.env.PORT || 5000
 var bodyParser = require('body-parser')
@@ -25,38 +26,43 @@ app.use(express.static('public'))
 
 app.post('/addname', (req, res) => {
   var myData = new User(req.body)
-  myData.save()
-    .then(item => {
-      res.sendFile(__dirname + '/response.html')
-    })
 
-    .catch(err => {
-      res.status(400).send('Unable to save to database')
+  if (validate.validateEmail(req.body.name)) {
+    myData.save()
+      .then(item => {
+        res.sendFile(__dirname + '/response.html')
+      })
+
+      .catch(err => {
+        res.status(400).send('Unable to save to database')
+      })
+    let transporter = nodeMailer.createTransport({
+      host: 'smtp.gmail.com',
+      port: 465,
+      secure: true,
+      auth: {
+        user: 'communitystaff@anitab.org',
+        pass: ''
+      }
     })
-  let transporter = nodeMailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
-    auth: {
-      user: 'communitystaff@anitab.org',
-      pass: ''
+    let mailOptions = {
+      from: '<communitystaff@anitab.org>', // sender address
+      to: req.body.name, // list of receivers
+      subject: 'Join the GHC slack channel !', // Subject line
+      text: 'Hell', // plain text body
+      html: '<img src ="http://abetteruserexperience.com/wp-content/uploads/2016/07/slack.png" > <br> <br><br><h1> Join GHC18 on Slack</h1><br><img src="https://ci5.googleusercontent.com/proxy/fyAoFvPyo0eYUKe4_VolitANLFrpYnGlA9dPRAn9Thn2P-RtFZBrniNkAu7FEmEg_j-XJ5-p40WabZ2smDC0Ava2DFzgnTXUyxPRRi5doXPnr-vKqfniYrZ7BdVMgjXh-2uPUhhLM3EK5qWWF6YjgV0n2yqQ0Q0u8Om6v6mWM0eh=s0-d-e1-ft#https://s3-us-west-2.amazonaws.com/slack-files2/avatars/2017-10-03/251165127045_b5f26f2478857e3c6557_88.jpg"><a href="https://ghc17.slack.com/"><h2>Click here to join the slack channel</h2></a>'
     }
-  })
-  let mailOptions = {
-    from: '<communitystaff@anitab.org>', // sender address
-    to: req.body.name, // list of receivers
-    subject: 'Join the GHC slack channel !', // Subject line
-    text: 'Hell', // plain text body
-    html: '<img src ="http://abetteruserexperience.com/wp-content/uploads/2016/07/slack.png" > <br> <br><br><h1> Join GHC18 on Slack</h1><br><img src="https://ci5.googleusercontent.com/proxy/fyAoFvPyo0eYUKe4_VolitANLFrpYnGlA9dPRAn9Thn2P-RtFZBrniNkAu7FEmEg_j-XJ5-p40WabZ2smDC0Ava2DFzgnTXUyxPRRi5doXPnr-vKqfniYrZ7BdVMgjXh-2uPUhhLM3EK5qWWF6YjgV0n2yqQ0Q0u8Om6v6mWM0eh=s0-d-e1-ft#https://s3-us-west-2.amazonaws.com/slack-files2/avatars/2017-10-03/251165127045_b5f26f2478857e3c6557_88.jpg"><a href="https://ghc17.slack.com/"><h2>Click here to join the slack channel</h2></a>'
+
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        return console.log(error)
+      }
+
+      res.render('index')
+    })
+  } else {
+    res.status(400).json('Invalid email')
   }
-
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      return console.log(error)
-    }
-
-    res.render('index')
-  })
 })
 
 app.listen(PORT, () => {
